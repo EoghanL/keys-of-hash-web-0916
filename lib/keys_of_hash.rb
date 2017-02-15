272c9fbb1a21ee3f6de10b6b7868850d3d8a978d
github username <github email address>
2017-02-15 18:32:53 -0500

    done

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,8 @@
+require 'pry-byebug'
 class Hash
-  def keys_of(arguments)
-    # code goes here
+  def keys_of(*arguments)
+    arguments.each_with_object([]) do |arg, results|
+      self.select { |key, val| results << key if val == arg }
+    end
   end
 end
